6cb7b7c1ff36da8bd285ebb6ee80a36b213a65bc
Sachin Kumar <sachindewan12@gmail.com>
2022-06-02 19:49:46 +0530

    Add files via upload

---
+++ kafka_consumer_dotnetcore/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace kafka_consumer_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("test");
+                while (true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
---
+++ microservice_producer_kafka/Controllers/ProducerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace microservice_producer_kafka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+        public ProducerController(ProducerConfig config)
+        {
+            this._config = config;
+        }
+        [HttpPost("send/{topic}")]
+        public async Task<ActionResult> Get(string topic, [FromBody] Employee employee)
+        {
+            string serializedEmployee = JsonConvert.SerializeObject(employee);
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
